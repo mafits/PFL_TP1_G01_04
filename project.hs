e8ab4bf55a0221761c97efae71bcd67a8f5c93c2
srissirs <sarareis2002@gmail.com>
2022-10-22 13:03:18 +0100

    comented code

--- project.hs
+++ project.hs
@@ -16,70 +16,92 @@ isEqual [] b = True
 isEqual (a:xs) b | exists a b = isEqual xs b
                  | otherwise = False
 
--- soma dois monómios (com variáveis e expoentes iguais)
+-- soma coeficientes de dois monómios (com variáveis e expoentes iguais)
 joinMon :: ([(String, Int)], [Int]) -> ([(String, Int)], [Int]) -> ([(String, Int)], [Int])
-joinMon x y = ( fst y, (snd y) ++ (snd x) )
+joinMon x y = ( fst y, [sum ((snd y) ++ (snd x))] )
 
--- soma vários monómios (com variáveis e expoentes iguais)
+-- soma os coeficientes de vários monómios (com variáveis e expoentes iguais)
 joinMonList :: [([(String, Int)], [Int])]  -> [([(String, Int)], [Int])]
 joinMonList (x:xs) = [foldl (\z y -> joinMon z y) x xs]
 
--- junta os monómios que têm var + exp iguais
+-- soma os monómios com variáveis e expoentes iguais dentro de um polinómio
 joinPoly :: [([(String, Int)], [Int])]  -> [([(String, Int)], [Int])]
 joinPoly [] = []
 joinPoly (x:xs) = (joinMonList ([x] ++ (filter (\y -> isEqual (fst x) (fst y)) xs))) ++ joinPoly (filter (\y -> not (isEqual (fst x) (fst y))) xs)
 
--- soma os coeficientes (da mesma lista)
-sumMon :: [([(String, Int)], [Int])] -> [([(String, Int)], [Int])]
-sumMon a = map (\x -> (fst x, [sum (snd x)])) a
-
--- remover monómios com coeficiente = 0
+-- remove os monómios com coeficiente zero ( == 0 ) ou sem coeficiente ( == [] )
 removeMon :: [([(String, Int)], [Int])] -> [([(String, Int)], [Int])]
 removeMon a = filter (\x -> (not (((snd x) !! 0) ==0) && ( not ((snd x)==[])))) a
+
+
 
 removeNull :: [(String, Int)] -> [(String, Int)]
 removeNull [] = []
 removeNull (x:xs) = if snd x == 0 then removeNull xs else x : removeNull xs
+
+removeNullExp :: ([(String, Int)], [Int]) -> ([(String, Int)], [Int])
+removeNullExp a = addEqualVar(removeNull(fst a), snd a)
 
 -- apagar e substituir por map
 removeVar :: [([(String, Int)], [Int])] -> [([(String, Int)], [Int])]
 removeVar xs = map removeNullExp xs
 
 
-removeNullExp :: ([(String, Int)], [Int]) -> ([(String, Int)], [Int])
-removeNullExp a = addEqualVar(removeNull(fst a), snd a)
+----   FUNÇÕES DE ORDENAÇÃO
 
-myPredicate (a1, a2) (b1, b2) = compare a1 b1 `mappend` compare a2 b2
-
-sortMon :: Ord a => Ord b => [(a, b)] -> [(a, b)]
-sortMon = sortBy (myPredicate) 
-
+--Função auxiliar : encontra o maior expoente de entre todas as incognitas de um monómio
 findMax:: [([Char] ,Int)]-> Int
 findMax [] = 0
 findMax [a] = snd a
 findMax (x:xs) = if( (snd x)>= (snd (head xs))) then findMax ([x]++(tail xs))
             else findMax xs
 
+-- Ordena a lista de incognitas por ordem crescente do seu expoente
+sortMon :: Ord a => Ord b => [(a, b)] -> [(a, b)]
+sortMon = sortBy (compare `on` snd)
+
+-- Ordena os monómios por ordem decrescente do seu maior expoente
 sortPol z = reverse(sortOn (\(x,y)->(findMax  x)) z) 
 
--- 1.
--- Normalize a polynomial
-normal :: [([(String, Int)], [Int])] -> [([(String, Int)], [Int])]
-normal a = sortPol (removeVar (removeMon (sumMon (joinPoly a))))
 
+----  FUNÇÕES PARA IMPRIMIR COMO STRING
+
+-- Passa as variáveis de monómio para string (x^2)
+stringifyVar :: [(String, Int)] -> String
+stringifyVar a = foldl (++) "" (map (\x -> if ((snd x) /= 1) then (fst x ++ "^" ++ (show (snd x))) else (fst x)) (sortMon a))
+
+-- Transforma um monómio em string
+stringifyMon :: ([(String, Int)], [Int]) -> String 
+stringifyMon ([], a) = show (a !! 0)
+stringifyMon a = show (abs ((snd a) !! 0)) ++ "*" ++ stringifyVar (fst a)
+
+-- Transforma um polinómio em string (adiciona os sinais entre os monómios )
+stringifyPol :: [([(String, Int)], [Int])] -> String
+stringifyPol a = foldl (++) "" (map (\x -> if (((snd x) !! 0) > 0 ) then (" + " ++ (stringifyMon x)) else (" - " ++ (stringifyMon x))) a)
+
+-- Verifica o valor do primeiro monómio e adiciona um "-" ou nada no início do polinómio
+stringify :: [([(String, Int)], [Int])] -> String
+stringify (x:xs) = if ((snd x) !! 0 > 0) then (stringifyMon x) ++ (stringifyPol xs) else "- " ++ (stringifyMon x) ++ (stringifyPol xs)
+
+-- 1. NORMALIZAÇÃO
+-- Normaliza um polinómio
+normal :: [([(String, Int)], [Int])] -> [([(String, Int)], [Int])]
+normal a = sortPol (removeVar (removeMon (joinPoly a)))
+
+-- Imprime o polinómio normalizado na forma de String
 stringifyNormal ::  [([(String, Int)], [Int])]  -> String
 stringifyNormal y = stringify  ( map (\x -> (sortMon (fst x), snd x)) (normal y))
 
--- 2.
--- sum two polynomials
+-- 2. SOMA
+-- Soma dois polinómios e imprime na forma de string
 sumPoly :: [([(String, Int)], [Int])] -> [([(String, Int)], [Int])] -> String
 sumPoly a b = stringify (normal (a ++ b))
 
--- sum multiple polynomials in a list
+-- Soma vários polinómios numa lista e imprime na forma de string
 sumPolyList :: [[([(String, Int)], [Int])]] -> String
 sumPolyList a = stringify (normal (foldl (++) [] a))
 
--- 3.
+-- 3. MULTIPLICAÇÃO
 -- multiplicar 2 mon
 mulMon :: ([(String, Int)], [Int]) -> ([(String, Int)], [Int]) -> ([(String, Int)], [Int])
 mulMon x y = (fst x ++ fst y, [(snd x)!!0 * (snd y)!!0])
@@ -114,22 +136,7 @@ mulPoly (x:xs) y = mulMonPoly x y ++ mulPoly xs y
 mult :: [([(String, Int)], [Int])] -> [([(String, Int)], [Int])] -> String
 mult a b =  stringify (normal (mulPoly a b))
 
--- passar variaveis de monomio para string (x^2)
-stringifyVar :: [(String, Int)] -> String
-stringifyVar a = foldl (++) "" (map (\x -> if ((snd x) /= 1) then (fst x ++ "^" ++ (show (snd x))) else (fst x)) (sortMon a))
 
--- passar monómio para string
-stringifyMon :: ([(String, Int)], [Int]) -> String 
-stringifyMon ([], a) = show (a !! 0)
-stringifyMon a = show (abs ((snd a) !! 0)) ++ "*" ++ stringifyVar (fst a)
-
--- passar polinómio para string
-stringifyPol :: [([(String, Int)], [Int])] -> String
-stringifyPol a = foldl (++) "" (map (\x -> if (((snd x) !! 0) > 0 ) then (" + " ++ (stringifyMon x)) else (" - " ++ (stringifyMon x))) a)
-
--- adicionar as operações entre os monómios
-stringify :: [([(String, Int)], [Int])] -> String
-stringify (x:xs) = if ((snd x) !! 0 > 0) then (stringifyMon x) ++ (stringifyPol xs) else "- " ++ (stringifyMon x) ++ (stringifyPol xs)
 
 -- 4. derivação
 -- mon não tem String -> 0
@@ -175,3 +182,7 @@ derivePol a b = stringify (normal (map (\x -> deriveMon x b) (normal a)))
 -- [([("x",9),("y",3)],[3,5]),([("x",7)],[9]),([("y",3),("x",1)],[7])]
 
 --[([("x",1),("y",3)],[3,5]),([("x",7)],[9]),([("y",3),("x",1)],[7])]
+
+
+--myPredicate (a1, a2) (b1, b2) = compare a1 b1 `mappend` compare a2 b2
+--sortMon = sortBy (myPredicate) 
